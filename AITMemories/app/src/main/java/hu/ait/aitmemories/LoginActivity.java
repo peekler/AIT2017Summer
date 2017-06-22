d9177b5627fdaf3f63737bbe22bc0e8cea8eeb90
Péter Ekler <peter.ekler@gmail.com>
2017-06-22 12:25:34 +0200

    AITMemories - Firebase application, Initial commit with authentication

---
+++ AITMemories/app/src/main/java/hu/ait/aitmemories/LoginActivity.java
@@ -0,0 +1,150 @@
+package hu.ait.aitmemories;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.etEmail)
+    EditText etEmail;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void loginClick() {
+        if (!isFormValid()) {
+            return;
+        }
+        
+        showProgressDialog();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                etEmail.getText().toString(),
+                etPassword.getText().toString()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        
+    }
+
+    @OnClick(R.id.btnRegister)
+    public void registerClick() {
+        if (!isFormValid()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                etEmail.getText().toString(), etPassword.getText().toString()
+        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                hideProgressDialog();
+
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "User created", Toast.LENGTH_SHORT).show();
+
+                    FirebaseUser fireBaseUser = task.getResult().getUser();
+
+                    fireBaseUser.updateProfile(new UserProfileChangeRequest.Builder().
+                            setDisplayName(getUserNameFromEmail(fireBaseUser.getEmail())).build());
+                } else {
+                    Toast.makeText(LoginActivity.this, "Not successs: "+
+                            task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                hideProgressDialog();
+
+                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        hideProgressDialog();
+        super.onStop();
+    }
+
+    public void showProgressDialog() {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Wait for it...");
+        }
+
+        progressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+    }
+
+    public boolean isFormValid() {
+        if (TextUtils.isEmpty(etEmail.getText().toString())) {
+            etEmail.setError(getString(R.string.error_field_empty));
+            return false;
+        }
+
+        if (TextUtils.isEmpty(etPassword.getText().toString())) {
+            etPassword.setError(getString(R.string.error_field_empty));
+            return false;
+        }
+
+        return true;
+     }
+
+     private String getUserNameFromEmail(String email){
+         if (email.contains("@")) {
+             return email.split("@")[0];
+         } else {
+             return email;
+         }
+     }
+
+
+}
